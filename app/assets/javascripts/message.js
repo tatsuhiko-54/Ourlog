1568192b56b30e6a9831cc0933ec9363bbde87de
tatsuhiko-54 <1998ta126@gmail.com>
2020-03-16 20:56:09 +0900

    autoload-d

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,9 +2,9 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-     `<div class="messages">
+     `<div class="messages" data-message-id="${message.id}">
      <div class="chat-main__messages__message">
-       <div class="chat-main__messages__message__upper-info">
+       <div class="chat-main__messags__message__upper-info">
          <div class="chat-main__messages__message__upper-info__talker">
          ${message.user_name}
          </div>
@@ -23,7 +23,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="messages">
+      `<div class="messages" data-message-id="${message.id}">
         <div class="chat-main__messages__message">
           <div class="chat-main__messages__message__upper-info">
             <div class="chat-main__messages__message__upper-info__talker">
@@ -43,18 +43,18 @@ $(function(){
      return html;
    };
  }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
   .done(function(data){
     var html = buildHTML(data);
     $(".chat-main__messages").append(html);
@@ -67,4 +67,32 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
    });
  })
+
+ var reloadMessages = function() {
+  var last_message_id = $('.messages:last').data("message-id");
+  console.log(last_message_id)
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log(messages)
+    if (messages.length !== 0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.chat-main__messages').append(insertHTML);
+    $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+   }
+  })
+  .fail(function() {
+    alert('error');
+  });
+ };
+ if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+ }
 });
