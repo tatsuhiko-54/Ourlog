84fa6216d20d63ae8247aca2c5779b2b70914f71
tatsuhiko-54 <1998ta126@gmail.com>
2020-04-25 23:10:42 +0900

    edit-css-2

---
+++ app/assets/javascripts/pop.js
@@ -0,0 +1,39 @@
+$(function(){
+
+  $(".fa-edit").on( 'mouseover', function(){
+    $(".chat-main__main-header__van").css( 'opacity', '1')
+    $(".chat-main__main-header__van").css('transition', '0.3s')
+  });
+  $(".fa-edit").on( 'mouseout', function(){
+    $(".chat-main__main-header__van").css( 'opacity', '0')
+    $(".chat-main__main-header__van").css('transition', '0.1s')
+  });
+
+  $(".fa-cog").on( 'mouseover', function(){
+    $(".pop-account").css( 'opacity', '1')
+    $(".pop-account").css('transition', '0.3s')
+  });
+  $(".fa-cog").on( 'mouseout', function(){
+    $(".pop-account").css( 'opacity', '0')
+    $(".pop-account").css('transition', '0.1s')
+  });
+
+  $(".fa-plus").on( 'mouseover', function(){
+    $(".pop-group").css( 'opacity', '1')
+    $(".pop-group").css('transition', '0.3s')
+  });
+  $(".fa-plus").on( 'mouseout', function(){
+    $(".pop-group").css( 'opacity', '0')
+    $(".pop-group").css('transition', '0.1s')
+  });
+
+
+
+
+
+
+
+
+
+
+});
